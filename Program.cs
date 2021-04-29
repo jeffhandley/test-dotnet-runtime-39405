e64add3eb7bfe4ee25f7970d59997ff0ab9febca
Jeff Handley <jeff.handley@microsoft.com>
2021-04-28 21:47:33 -0700

    Update to match new implementation; generate side-by-side output

--- Program.cs
+++ Program.cs
@@ -9,38 +9,44 @@ namespace animatorapp
     class Program
     {
         static string OutputFolder;
-        static Bitmap GIF;
+        static Bitmap GIF_Local;
+        static Bitmap GIF_Main;
 
         static void Main(string[] args)
         {
             if (args.Length != 2 || !File.Exists(args[0]))
             {
-                throw new ArgumentException("Syntax: appanimator imageFile outputFolder");
+                throw new ArgumentException("Syntax: appanimator imageFile outputFolder. " + string.Join(",", args));
             }
 
             string now = DateTime.Now.ToString("HH-mm-ss-ff");
             OutputFolder = Path.Combine(args[1], now);
             Directory.CreateDirectory(OutputFolder);
 
-            GIF = new(args[0]);
-            System.Drawing.ImageAnimator.Animate(GIF, new EventHandler(OnAnimated));
+            GIF_Local = new(args[0]);
+            GIF_Main = new(args[0]);
+
+            ImageAnimator.Animate(GIF_Local, new EventHandler(OnAnimated));
+            System.Drawing.ImageAnimator.Animate(GIF_Main, new EventHandler(OnAnimated));
 
             for (var frame = 0; frame < 1000; frame++)
             {
                 SaveSnapshot();
                 Thread.Sleep(10);
             }
-
-            //ImageAnimator.StopAnimate(GIF, new EventHandler(OnAnimated));
         }
 
         private static void SaveSnapshot()
         {
             string now = DateTime.Now.ToString("HH-mm-ss-ff");
+            ImageAnimator.UpdateFrames();
             System.Drawing.ImageAnimator.UpdateFrames();
-            Image thumbnail = GIF.GetThumbnailImage(128, 128, null, IntPtr.Zero);
 
-            thumbnail.Save(Path.Combine(OutputFolder, $"{now}.png"), ImageFormat.Png);
+            Image thumbnail_local = GIF_Local.GetThumbnailImage(128, 128, null, IntPtr.Zero);
+            thumbnail_local.Save(Path.Combine(OutputFolder, $"{now}_local.png"), ImageFormat.Png);
+
+            Image thumbnail_main = GIF_Main.GetThumbnailImage(128, 128, null, IntPtr.Zero);
+            thumbnail_main.Save(Path.Combine(OutputFolder, $"{now}_main.png"), ImageFormat.Png);
         }
 
         private static void OnAnimated(object o, EventArgs e)
